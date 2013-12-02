892742f0066b65146e0a845a4060e418c1b77efb
Floc95 <florianconstant@gmail.com>
2013-12-02 15:51:26 +0100

    [DAL] Ajout de la couche d'accès aux données

---
+++ src/com/esgi/studyingfurther/dal/Repository.java
@@ -0,0 +1,30 @@
+package com.esgi.studyingfurther.dal;
+
+import com.esgi.studyingfurther.bl.*;
+
+public class Repository {
+
+	public Repository()
+	{
+	}
+	
+	public User getUser(int id)
+	{
+		// TODO getUser
+		// Chargement d'un utilisateur en fonction d'un id
+		// Retourne Null si l'id n'existe pas
+		return null;
+	}
+	
+	public User getUser(String login, String password)
+	{
+		// TODO getUser
+		// Chargement d'un utilisateur en fonction d'un login et d'un mot de passe
+		// Retourne Null s'il n'existe pas
+		return null;
+	}
+	
+	// D'autres méthodes utiles seront ajoutés
+	// (obtention des posts, des commentaires, des utilisateurs, des groupes, etc.)
+	
+}
