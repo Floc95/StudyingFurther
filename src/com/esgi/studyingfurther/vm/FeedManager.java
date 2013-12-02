9d65197e6d41884e78df8fa12b4ad0e5999a5004
Floc95 <florianconstant@gmail.com>
2013-12-02 16:21:52 +0100

    [VM] Ajout des ViewModel
    Ajout d'un MainViewModel, gérant toute l'application.
    Ajout d'un FeedManager, gérant les posts et commentaires.

---
+++ src/com/esgi/studyingfurther/vm/FeedManager.java
@@ -0,0 +1,16 @@
+package com.esgi.studyingfurther.vm;
+
+import com.esgi.studyingfurther.bl.Factory;
+
+public class FeedManager {
+
+	private Factory factory;
+	
+	public FeedManager(Factory factory)
+	{
+		this.factory = factory;
+	}
+	
+	// Faire toutes les méthodes pour gérer les posts
+	
+}
---
+++ src/com/esgi/studyingfurther/vm/MainViewModel.java
@@ -0,0 +1,30 @@
+package com.esgi.studyingfurther.vm;
+
+import com.esgi.studyingfurther.bl.Factory;
+import com.esgi.studyingfurther.bl.User;
+
+public class MainViewModel {
+
+	private Factory factory;
+	private User currentUser;
+	private FeedManager feedManager;
+	
+	public MainViewModel(Factory factory)
+	{
+		this.factory = factory;
+		this.feedManager = new FeedManager(factory);
+	}
+	
+	public boolean Authenticate(String login, String password)
+	{
+		User user = this.factory.getUser(login, password);
+		if (user != null)
+		{
+			this.currentUser = user;
+		}
+		return user != null ? true : false;
+	}
+	
+	// Faire toutes les autres méthodes utiles à l'UI : getPosts, comments etc...
+	
+}
