32241a5bfa55cbceed3fde02c8b047f19c110f10
mohammedelakraoui <mohammed.elakraoui@gmail.com>
2014-03-11 14:05:30 +0100

    Authentification

--- src/com/esgi/studyingfurther/Identification.java
+++ src/com/esgi/studyingfurther/Identification.java
@@ -3,7 +3,13 @@ package com.esgi.studyingfurther;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static java.lang.String.*;
 
 public class Identification extends Activity {
 
@@ -11,6 +17,7 @@ public class Identification extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_identification);
+
 	}
 
 	@Override
@@ -20,10 +27,29 @@ public class Identification extends Activity {
 		return true;
 	}
 
-	void validation(){
-		
-		Intent intent = new Intent(this, NewsFeed.class);
 
+
+	public void SeConnecter(View v){
+
+		if(this.Authentification("SIMO","SIMO")==true)
+        {
+		Intent intent = new Intent(this, NewsFeed.class);
 		startActivity(intent);
+        }
+
+
 	}
+
+    public boolean Authentification(String ID, String Password)
+    {
+        String username= ((EditText) findViewById(R.id.username)).getText().toString();
+        String Pass= ((EditText) findViewById(R.id.username)).getText().toString();
+      Log.v("info",Pass+":"+username);
+
+         if(ID.equals(username) && Password.equals(Pass))
+         {
+             return  true;
+         }
+        return false;
+    }
 }
