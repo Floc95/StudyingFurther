e3fdeecb34d1a806cd794f9f6ebc1a286a469f6a
mohammedelakraoui <mohammed.elakraoui@gmail.com>
2014-03-21 23:05:18 +0100

    Formulaire inscription

---
+++ src/com/esgi/studyingfurther/Inscription.java
@@ -0,0 +1,27 @@
+package com.esgi.studyingfurther;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+/**
+ * Created by server-pc on 13/03/14.
+ */
+public class Inscription extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+      //  TextView t=(TextView)findViewById(R.id.message);
+      //  t.setText("OUI");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        return true;
+    }
+
+}
