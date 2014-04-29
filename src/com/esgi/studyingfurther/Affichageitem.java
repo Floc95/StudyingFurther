4923c6c80737591e38009fd27219c6c784d4de05
wqwj <alan.q811@gmail.com>
2014-04-30 00:04:28 +0800

    Les activities pour parameter, affichageItem et gererClass

--- src/com/esgi/studyingfurther/Affichageitem.java
+++ src/com/esgi/studyingfurther/Affichageitem.java
@@ -1,22 +1,163 @@
 package com.esgi.studyingfurther;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
-public class Affichageitem extends Activity {
+public class AffichageItem extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_affichageitem);
-	}
+    TextView titre;
+    TextView description;
+    TextView owner;
+    TextView time;
+    Button button, download;
+    SimpleAdapter listItemAdapter;
+    ArrayList<HashMap<String, Object>> listItem;
+    private ListView myList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_post);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.affichageitem, menu);
-		return true;
-	}
+        myList = (ListView)findViewById(R.id.listviewperso);
+        button = (Button)findViewById(R.id.send);
+        button.setEnabled(false);
+        titre = (TextView)this.findViewById(R.id.titre);
+        description = (TextView)this.findViewById(R.id.description);
+
+        listItem = new ArrayList<HashMap<String, Object>>();
+
+
+        HashMap<String, Object> map = new HashMap<String, Object>();
+
+        map.put("title", "Comment "+ listItem.size());
+
+        listItem.add(map);
+        addItem("titile1","content1");
+        addItem("owner1","content2");
+        addItem("owner2","content3");
+        addItem("owner3","content3");
+        addItem("owner4","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+        addItem("owner5","content3");
+
+        HashMap<String, Object> tmp = new HashMap<String, Object>();
+        tmp.put("title", "Comment  "+ listItem.size());
+        listItem.set(0, tmp);
+
+        listItemAdapter = new SimpleAdapter(this.getBaseContext(), listItem, R.layout.activity_affichageitem, new String[]{"image", "title", "text"}, new int[]{R.id.img, R.id.titre, R.id.description});
+        myList.setAdapter(listItemAdapter);
+
+
+        myList.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                ((InputMethodManager)getSystemService(INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(AffichageItem.this.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                button.setEnabled(false);
+                return false;
+            }
+        });
+
+
+        final EditText coment = (EditText)findViewById(R.id.editText1);
+        coment.setOnTouchListener(new OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                button.setEnabled(true);
+                return false;
+            }
+        });
+
+
+        coment.setOnFocusChangeListener(new OnFocusChangeListener() {
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus)
+                {
+                    ((InputMethodManager)getSystemService(INPUT_METHOD_SERVICE)).showSoftInput(coment, 0);
+                }
+
+            }
+        });
+
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (!coment.getText().toString().trim().equals(""))
+                {
+                    addItem("new",coment.getText().toString());
+                    coment.setText("");
+                    listItemAdapter.notifyDataSetChanged();
+                }
+                else {
+                    new AlertDialog.Builder(AffichageItem.this)
+                            .setTitle("Warning")
+                            .setMessage("Comment can't be empty")
+                            .setPositiveButton("OK", null)
+                            .show();
+                }
+
+
+            }
+        });
+
+        download = (Button)findViewById(R.id.download);
+        download.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AffichageItem.this, Parameter.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void addItem(String title, String text)
+    {
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("image", R.drawable.android);
+        map.put("title", title);
+        map.put("text", text);
+        listItem.add(map);
+//        listItemAdapter.notifyDataSetChanged();
+    }
+
 
 }
---
+++ src/com/esgi/studyingfurther/Parameter.java
@@ -0,0 +1,158 @@
+package com.esgi.studyingfurther;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.http.*;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class Parameter extends Activity {
+
+	private ListView myList;
+	ArrayList<HashMap<String, Object>> listItem;
+	SimpleAdapter listItemAdapter;
+	CheckBox cb;
+	private static String url="";
+	NotificationManager m_NotificationManager;  
+    Notification m_Notification;  
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_parameter);
+		
+		myList = (ListView)findViewById(R.id.listPara);
+		listItem = new ArrayList<HashMap<String, Object>>(); 
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "Gerer mes classes");
+        listItem.add(map);
+        map = new HashMap<String, Object>();
+        map.put("title", "Se deconnecter");
+        listItem.add(map);
+        listItemAdapter = new SimpleAdapter(this.getBaseContext(), listItem, R.layout.listparameter, new String[]{ "title"}, new int[]{R.id.textPara});
+        myList.setAdapter(listItemAdapter);
+        
+        cb = (CheckBox)findViewById(R.id.cb);
+        cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) 
+				{
+//					getServerData(url);
+					addNotificaction();
+				}
+				else {
+					m_NotificationManager.cancel(1);
+				}
+			}
+		});
+        
+        myList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				if (arg2 == 0) 
+				{
+					Intent intent = new Intent(Parameter.this,GererClass.class);
+					startActivity(intent);
+				}
+				else
+				{
+					new AlertDialog.Builder(Parameter.this)
+					.setTitle("Warning")
+					.setMessage("deco")
+					.setPositiveButton("OK", null)
+					.show();
+					
+				}
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.parameter, menu);
+		return true;
+	}
+	/*
+	private void getServerData(String url)
+	{
+		url += "?username=123456";
+		HttpClient client = new DefaultHttpClient();
+		HttpPost request; 
+		try 
+		{
+			request = new HttpPost(new URI(url));
+			HttpResponse response=client.execute(request);
+			
+			if(response.getStatusLine().getStatusCode()==200)
+			{
+				HttpEntity entity=response.getEntity();
+				if(entity!=null)
+				{
+					String out = EntityUtils.toString(entity);
+					new AlertDialog.Builder(this).setMessage(out).create().show();
+				}
+			}
+		}
+		catch (URISyntaxException e) 
+		{
+			e.printStackTrace();
+		}
+		catch (ClientProtocolException e) 
+		{
+			e.printStackTrace();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	*/
+	
+	@SuppressWarnings("deprecation")
+	private void addNotificaction() 
+	{  
+        m_NotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);  
+        m_Notification = new Notification();
+        m_Notification.when = System.currentTimeMillis();
+        m_Notification.icon = R.drawable.android;  
+        m_Notification.tickerText = "je suis la";  
+        m_Notification.defaults = Notification.DEFAULT_ALL;  
+        m_Notification.flags = Notification.FLAG_ONGOING_EVENT;
+  
+        Intent intent = new Intent(this, AffichageItem.class);  
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);  
+        m_Notification.setLatestEventInfo(this, "NotificationTitle��", "Notification Content", pendingIntent);  
+        m_NotificationManager.notify(1, m_Notification);  
+          
+    }  
+}
