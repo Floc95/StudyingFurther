4155feef10f39126612f361ed9c0f4a4279d752d
mohammed.elakraoui@gmail.com <mohammed.elakraoui@gmail.com>
2014-06-19 09:54:51 +0200

    news feed

---
+++ src/com/esgi/studyingfurther/vm/MyViewBinder.java
@@ -0,0 +1,27 @@
+package com.esgi.studyingfurther.vm;
+
+
+import android.graphics.Bitmap;
+import 
+android.widget.SimpleAdapter.ViewBinder;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MyViewBinder implements ViewBinder {
+
+	@Override
+	public boolean setViewValue(View view, Object data, String arg2) {
+		// TODO Auto-generated method stub
+		if( (view instanceof ImageView) & (data instanceof Bitmap) ) {
+			ImageView iv = (ImageView) view;
+			Bitmap bm = (Bitmap) data;	
+			iv.setImageBitmap(bm);	
+			return true;
+		}
+ 
+		return false;
+	}
+
+	
+
+}
