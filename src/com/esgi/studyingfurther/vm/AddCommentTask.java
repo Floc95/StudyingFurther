9013d4b4c955d54d937f5e18a502ba66d5e2f8d7
mohammedelakraoui <mohammed.elakraoui@gmail.com>
2014-06-22 11:23:17 +0200

    add comment

---
+++ src/com/esgi/studyingfurther/vm/AddCommentTask.java
@@ -0,0 +1,44 @@
+package com.esgi.studyingfurther.vm;
+
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+
+
+public class AddCommentTask  extends AsyncTask<String, Void, String>{
+
+	  protected String doInBackground(String... urls) {
+		  
+		   //get url of of request comment
+		   String urldisplay = urls[0];
+	     //  String decodeMyUrl=  Uri.encode(urldisplay,"UTF-8");
+			 Log.v("url",urldisplay);
+		 //create instance of InputStream and pass URL
+		    try {
+				InputStream in = new URL(urldisplay).openStream();
+				
+			    return String.valueOf(in.read());
+			
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		  
+		   return "error";
+		  }
+		 
+		  protected void onPostExecute(String result) {
+		  
+		  }
+}
